a0fae393d691803f00523550e31921b428f499f8
Michael Parascandolo <michaelparascandolo@me.com>
2023-04-28 17:56:02 -0400

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
+
+interface WeatherData {
+  name: string;
+  main: {
+    temp: number;
+    feels_like: number;
+    humidity: number;
+    temp_max: number;
+    temp_min: number;
+  };
+  weather: {
+    description: string;
+    main: string;
+    icon: string;
+  }[];
+  sys: {
+    country: string;
+  };
+  wind: {
+    speed: number;
+    gust: number;
+  };
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
+// https://api.openweathermap.org/data/2.5/weather?q=Tuckerton&units=imperial&appid=6e21e21d00dac27b8e466eb450211833
 export class AppComponent {
-  title = 'weather-angular';
+  apiKey: string = '6e21e21d00dac27b8e466eb450211833';
+  weatherData: WeatherData | undefined;
+  inputValue = '';
+  minTemp = '';
+
+  constructor(private http: HttpClient) {}
+
+  public search = (inputValue: string | number, currentLocation: boolean) => {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&units=imperial&appid=${this.apiKey}`;
+    this.http
+      .get<WeatherData>(apiUrl)
+      .pipe(
+        tap((response: WeatherData | undefined) => {
+          this.weatherData = response;
+          // console.log(this.weatherData);
+        })
+      )
+      .subscribe();
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, FooterComponent, LogoComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
