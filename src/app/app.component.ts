8f6cfed4943320426b81405ea99bbdbf0da1aa24
Michael Parascandolo <michaelparascandolo@me.com>
2023-04-29 16:07:42 -0400

    search does not work if field is null

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,45 +41,45 @@ export class AppComponent {
   constructor(private http: HttpClient) {}
 
   public search = (inputValue: string | number, currentLocation: boolean) => {
-    console.log(inputValue);
-    let units: string;
-    let searchHow: string;
-    let latitude;
-    let longitude;
-    if (currentLocation == true) {
-      searchHow = searchHow = '&lat=' + latitude + '&lon=' + longitude;
-    } else {
-      if (typeof inputValue === 'number') {
-        searchHow = 'zip=';
+    if (inputValue != '') {
+      this.weatherData = undefined;
+      console.log(inputValue);
+      let units: string;
+      let searchHow: string;
+      let latitude;
+      let longitude;
+      if (currentLocation == true) {
+        searchHow = searchHow = '&lat=' + latitude + '&lon=' + longitude;
       } else {
-        searchHow = 'q=';
+        if (typeof inputValue === 'number') {
+          searchHow = 'zip=';
+        } else {
+          searchHow = 'q=';
+        }
       }
-    }
-    if (this.imperial == false) {
-      units = 'imperial';
-    } else {
-      units = 'metric';
-    }
+      if (this.imperial == false) {
+        units = 'imperial';
+      } else {
+        units = 'metric';
+      }
 
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?${searchHow}${inputValue}&units=${units}&appid=${this.apiKey}`;
-    this.http
-      .get<WeatherData>(apiUrl)
-      .pipe(
-        tap((response: WeatherData | undefined) => {
-          this.weatherData = response;
-        }),
-        catchError((error: any) => {
-          console.log('SOMETHING WENT WRONG');
-          this.placeHolder = 'CITY NOT FOUND!';
-          this.inputValue = '';
-          return error;
-        })
-      )
+      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?${searchHow}${inputValue}&units=${units}&appid=${this.apiKey}`;
+      this.http
+        .get<WeatherData>(apiUrl)
+        .pipe(
+          tap((response: WeatherData | undefined) => {
+            this.placeHolder = 'Search City | ZIP Code (US)';
+            this.weatherData = response;
+          }),
+          catchError((error: any) => {
+            console.log('SOMETHING WENT WRONG');
+            this.placeHolder = 'CITY NOT FOUND!';
+            this.inputValue = '';
+            return error;
+          })
+        )
 
-      .subscribe();
+        .subscribe();
+    }
   };
-  // used for testing
-  ngOnInit() {
-    // this.search('New York', false);
-  }
 }
