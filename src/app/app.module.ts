27c81e6557e3f79cf556cff864b5f9a2209e4955
Michael Parascandolo <michaelparascandolo@me.com>
2023-05-11 11:33:35 -0400

    creating new card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LogoComponent } from './components/logo/logo.component';
 import { FormsModule } from '@angular/forms';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
-  declarations: [AppComponent, FooterComponent, LogoComponent],
+  declarations: [AppComponent, FooterComponent, LogoComponent, CardComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
