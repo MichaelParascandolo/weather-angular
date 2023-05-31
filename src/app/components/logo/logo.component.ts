2e2a619ed1de1102868b6b0c340f9a947b9759b8
Michael Parascandolo <michael.parascandolo@me.com>
2023-05-30 23:03:05 -0400

    deleting unused files

--- src/app/components/logo/logo.component.ts
+++ src/app/components/logo/logo.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-logo',
-  templateUrl: './logo.component.html',
+  // templateUrl: './logo.component.html',
+  template:
+    '<div style="width: 100%; display: flex; justify-content: center"><div class="title">WeatherMate<i class="fa-solid fa-bolt" style="color: #2196f3"></i></div></div>',
   styleUrls: ['./logo.component.css'],
 })
 export class LogoComponent {}
